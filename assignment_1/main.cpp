33662a5c1ed551473c895fae6bda2cac879f787c
CoolerSonic91 <ianpaul.com@gmail.com>
2024-08-27 19:55:57 +0200

    Changes in assignment 1

--- assignment_1/main.cpp
+++ assignment_1/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main () {
     double celsius, fahrenheit;
-    cout << "Give me a temperature in Celsius" << endl;
+    cout << "Give me a temperature in Celsius!!" << endl;
     cin >> celsius;
     fahrenheit = (9.0 / 5.0 ) * celsius + 32;
     cout << celsius << " Celsius is " << fahrenheit << " Fahrenheit.";
